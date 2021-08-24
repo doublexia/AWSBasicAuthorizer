aabad7823f5df46069b36046f744ae3f191f656d
doublexia <doublexia@gmail.com>
2021-08-24 13:03:57 -0600

    Update FileUploader.java

--- FileUploader.java
+++ FileUploader.java
@@ -17,6 +17,25 @@ public class FileUploader {
         OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream(), StandardCharsets.UTF_8);
         out.write(content);
         out.close();
+        connection.disconnect();
+
+        return connection.getResponseCode();
+    }
+    
+    public static int uploadBase64StringToPresignedUrl(String presignedURL, String base64EncodedContent) throws IOException {
+        URL url = new URL(presignedURL);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Content-Type","application/octet-stream");
+        connection.setRequestProperty("x-amz-server-side-encryption","AES256");
+        connection.setRequestMethod("PUT");
+        
+        ByteArrayOutputStream byteArrayStream = new ByteArrayOutputStream();
+        byteArrayStream.write(Base64.getDecoder().decode(encodedString));
+        byteArrayStream.writeTo(connection.getOutputStream());
+        
+        byteArrayStream.close();
+        connection.disconnect();
 
         return connection.getResponseCode();
     }
